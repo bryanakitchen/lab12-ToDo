1562631fca5b6c4fe6ecb5895b44406a83fb6296
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-11-10 19:18:26 -0500

    initial commit

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    chore: 'wash the dishes',
+    completed: false,
+  },
+  {
+    chore: 'vacuum',
+    completed: false,
+  },
+  {
+    chore: 'take out the trash',
+    completed: false,
+  }
+];
